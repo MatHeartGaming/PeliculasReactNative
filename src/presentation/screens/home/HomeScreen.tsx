a56fb3df51586f2312ee4c27c4ae882c542be97c
Matteo Buompastore <matteo.buompastore088@gmail.com>
2025-04-16 11:24:35 +0200

    Created NowPlaying ScrollView

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import { useMovies } from '../../hooks/useMovies'
+import { ScrollView } from 'react-native-gesture-handler'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { PosterCarousel } from '../../components/movies/PosterCarousel'
 
 export const HomeScreen = () => {
-  const {} = useMovies()
+  const { top } = useSafeAreaInsets()
+  const { isLoading, nowPlaying } = useMovies()
+
+  if (isLoading) {
+    return () => {
+      <Text>Loading...</Text>
+    }
+  }
+
   return (
-    <View>
-        <Text>Home</Text>
-    </View>
+    <ScrollView>
+      <View style={{ marginTop: 10, paddingBottom: 30 }}>
+
+        <PosterCarousel movies={nowPlaying} />
+
+      </View>
+    </ScrollView>
   )
 }
