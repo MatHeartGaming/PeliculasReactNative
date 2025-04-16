f3b27067b44a03ed087d15e658fe321c62978c53
Matteo Buompastore <matteo.buompastore088@gmail.com>
2025-04-16 11:51:28 +0200

    Flat Lists

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -4,15 +4,14 @@ import { useMovies } from '../../hooks/useMovies'
 import { ScrollView } from 'react-native-gesture-handler'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { PosterCarousel } from '../../components/movies/PosterCarousel'
+import { HorizontalCarousel } from '../../components/movies/HorizontalCarousel'
 
 export const HomeScreen = () => {
-  const { top } = useSafeAreaInsets()
-  const { isLoading, nowPlaying } = useMovies()
+  const { top } = useSafeAreaInsets();
+  const { isLoading, nowPlaying, popular, topRated, upcoming } = useMovies();
 
-  if (isLoading) {
-    return () => {
-      <Text>Loading...</Text>
-    }
+  if (false) {
+    return <Text>Loading...</Text>
   }
 
   return (
@@ -20,6 +19,21 @@ export const HomeScreen = () => {
       <View style={{ marginTop: 10, paddingBottom: 30 }}>
 
         <PosterCarousel movies={nowPlaying} />
+
+        <HorizontalCarousel
+          movies={popular}
+          title='Popular'
+        />
+
+        <HorizontalCarousel
+          movies={topRated}
+          title='Top Rated'
+        />
+
+        <HorizontalCarousel
+          movies={upcoming}
+          title='Proximamente'
+        />
 
       </View>
     </ScrollView>
